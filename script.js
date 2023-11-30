d592b140c76496e02366951daae41cbfac9c16ea
MiniEnemy <home.minj@gmail.com>
2023-11-30 14:16:03 +0545

    day1

---
+++ script.js
@@ -0,0 +1,47 @@
+// ;document.write("hello js");
+// for (variable in Object){
+
+// }
+// var person={"first name":"jhon","lastname":"nash","age":"64"}
+// for(var prop in person){
+//     document.write("<p>"+prop+"="+ person[prop]+"</p>")
+// }
+// let greet="Hello world"
+// for(let character of greet){
+//     document.write(character);
+// }
+// function getSum(n1,n2){
+//     if(!Number.isInteger(n1) || !Number.isInteger(n2)){
+//         alert("not a number");
+//         return n1+n2;
+//     }
+// }
+
+// var a=parseInt(prompt("ENter first number"));
+// var b=parseInt(prompt("ENter second number"));
+// document.write(getSum(a,b));
+// var a1=2;
+// function getSum(a1){
+//     if(a1<10){
+//         alert("not a number");
+//     }
+//     return a1;
+// }
+// function getSum(a1) {
+//     if (isNaN(a1)) {
+//         alert("not a number");
+//     }
+//     return a1;
+// }
+var age = 15;
+
+if (age < 18) {
+    alert("You are less than 18 years old.");
+} else {
+    alert("You are 18 years old or older.");
+}
+
+
+// var a=parseInt(prompt("ENter first number"));
+// var b=parseInt(prompt("ENter second number"));
+document.write(getSum(20));
